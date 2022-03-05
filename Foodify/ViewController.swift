9f467c3a5d76ce4e141fdf801215f31551b43ca5
Nwachukwu <nwachukwu@cowrywise.com>
2022-03-05 13:44:07 +0100

    Project and launch screen setup

---
+++ Foodify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodify
+//
+//  Created by Nwachukwu Ejiofor on 05/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
